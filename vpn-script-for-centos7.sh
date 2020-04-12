6d7dc1762012bd88382f6a1a365310e33fae7938
xiedengmin <31821015+xiedengmin@users.noreply.github.com>
2020-04-12 21:55:45 +0800

    Update vpn-script-for-centos7.sh

--- vpn-script-for-centos7.sh
+++ vpn-script-for-centos7.sh
@@ -32,7 +32,7 @@ printf "
 #                                                           #
 #############################################################
 "
-
+yum install net-tools
 #获取服务器IP
 serverip=$(ifconfig -a |grep -w "inet"| grep -v "127.0.0.1" |awk '{print $2;}')
 printf "\e[33m$serverip\e[0m is the server IP?"
@@ -140,7 +140,6 @@ mknod /dev/random c 1 9
 
 #更新组件
 #yum update -y
-yum install net-tools
 #安装epel源
 yum install epel-release -y
 
